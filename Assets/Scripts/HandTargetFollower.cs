8ae1e2b327055f609282c791d3efc8a02a26d4f4
YooN <70580990+yoonyjin@users.noreply.github.com>
2025-07-13 23:34:57 +0900

    Reapply "Merge branch 'main' into YeoJin2"
    This reverts commit fdbe07445e3cb9eaf6ded8cefb6ac0a2299a7ce3.

--- Assets/Scripts/HandTargetFollower.cs
+++ Assets/Scripts/HandTargetFollower.cs
@@ -6,6 +6,13 @@ public class HandTargetFollower : MonoBehaviourPun
     public Transform source;
     public Vector3 eulerOffset = new(90, 0, 0);
 
+    private void Awake()
+    {
+        if(!photonView.IsMine)
+        {
+            this.enabled = false;
+        }
+    }
     void LateUpdate()
     {
         //if (photonView.IsMine && source != null)
