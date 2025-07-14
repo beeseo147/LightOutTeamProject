5ff3e20e386f8065ea35673e9783bc02a1bdbd74
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2025-07-14 12:38:46 +0900

    "PlayerSetUp"

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -8,28 +8,36 @@ using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class PlayerSetup : MonoBehaviourPun
 {
-    [SerializeField] GameObject xrOrigin;        // XR Origin �Ѹ�
-    [SerializeField] GameObject avatarVisuals;   // �� ��ü(�޽� ����)
-    [SerializeField] GameObject leftHandModel;
-    [SerializeField] GameObject rightHandModel;
+    [SerializeField] GameObject xrOrigin;        // XR Origin  Ѹ 
+    [SerializeField] GameObject avatarVisuals;
+    //[SerializeField] GameObject cameraOffset;
+    //[SerializeField] GameObject locomotion;
+    //[SerializeField] GameObject leftHandModel;
+    //[SerializeField] GameObject rightHandModel;
 
     void Awake()
     {
         if (photonView.IsMine)
         {
-            avatarVisuals.SetActive(false);
+            //avatarVisuals.SetActive(false);
+            xrOrigin.SetActive(true);
+
             //var myCam = xrOrigin.GetComponentInChildren<Camera>(true);
             //if (myCam != null)
             //    myCam.gameObject.SetActive(true);
             //xrOrigin.SetActive(true);
-            //SetLayerRecursively(avatarVisuals, LayerMask.NameToLayer("LocalBody"));
+            SetLayerRecursively(avatarVisuals, LayerMask.NameToLayer("LocalBody"));
         }
         else
         {
+
+            //avatarVisuals.SetActive(true);
+            SetLayerRecursively(avatarVisuals, LayerMask.NameToLayer("RemoteBody"));
+            //cameraOffset.SetActive(false);
+            //locomotion.SetActive(false);
             //leftHandModel.SetActive(false);
             //rightHandModel.SetActive(false);
-            avatarVisuals.SetActive(true);
-            xrOrigin.SetActive(false);
+            //avatarVisuals.SetActive(true);
             //GetComponentInChildren<LocomotionSystem>().enabled = false;
             //GetComponentInChildren<InputActionManager>().enabled = false;
             //var cam = xrOrigin.GetComponentInChildren<Camera>(true);
@@ -44,13 +52,12 @@ public class PlayerSetup : MonoBehaviourPun
             //foreach (var tpd in trackedPoseDrivers)
             //    tpd.enabled = false;
             //xrOrigin.SetActive(false);
-            //SetLayerRecursively(avatarVisuals, LayerMask.NameToLayer("RemoteBody"));
         }
     }
 
     private void Update()
     {
-        if(!photonView.IsMine)
+        if (!photonView.IsMine)
         {
 
         }
@@ -59,6 +66,6 @@ public class PlayerSetup : MonoBehaviourPun
     void SetLayerRecursively(GameObject obj, int layer)
     {
         obj.layer = layer;
-        foreach(Transform t in obj.transform) SetLayerRecursively(t.gameObject, layer);
+        foreach (Transform t in obj.transform) SetLayerRecursively(t.gameObject, layer);
     }
 }
